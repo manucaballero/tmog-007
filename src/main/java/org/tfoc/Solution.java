ce03fab8faf9473d21123f184ef4aeb7a2e97a97
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-03-06 20:39:13 +0100

    Solved galadriel 007

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -5,11 +5,43 @@ package org.tfoc;
  */
 public class Solution {
 
-    public boolean isAnagram(
+    public static boolean isAnagram(
             String s,
             String t) {
 
-        return false;
+        if(s.length() != t.length()){
+
+            return false;
+        }
+
+        int[] firstWordData = fillLetterOcurrences(s);
+        int[] secondWordData = fillLetterOcurrences(t);
+
+        boolean areEquals = true;
+        int counter = 0;
+        while (counter < firstWordData.length && areEquals){
+
+            if(firstWordData[counter] != secondWordData[counter]){
+                areEquals = false;
+            }
+
+            counter++;
+        }
+
+        return areEquals;
+    }
+
+    private static int[] fillLetterOcurrences(String word){
+
+        int[] wordData = new int[26];//26 -> Number of english lowercase characters
+
+        for(int i = 0; i < word.length(); i++){
+
+            char currentCharacter = word.charAt(i);
+            int letterPos = currentCharacter - 'a';
+            wordData[letterPos]++;
+        }
+        return wordData;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,34 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void anagramSolution01(){
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        boolean result = Solution.isAnagram("anagram","nagaram");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void anagramSolution02(){
+
+        boolean result = Solution.isAnagram("rat","art");
+
+        assertTrue(result);
+    }
+
+    @Test
+    void notAnagramSolution01(){
+
+        boolean result = Solution.isAnagram("rat","car");
+
+        assertFalse(result);
     }
 
 }
